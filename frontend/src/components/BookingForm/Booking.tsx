595f4a7b2b723f8038541ba8263811fdbc0cd6a7
AryanBhandary <bhaaryan304@gmail.com>
2025-09-25 01:00:54 +0545

    Fixed import error in Booking Form.

--- frontend/src/components/BookingForm/Booking.tsx
+++ frontend/src/components/BookingForm/Booking.tsx
@@ -1,17 +1,13 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"; // Removed useNavigate
 import rooms from "../../constants/roomsData";
 import { useState, useEffect } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react"; // arrow icons
 import Form from "./Form";
 
 function Booking() {
     const { id } = useParams();
-    const navigate = useNavigate();
     const room = rooms.find((r) => r.id === Number(id));
-
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    // 🔥 Automatic slider
     useEffect(() => {
         if (!room) return;
 
@@ -33,35 +29,25 @@ function Booking() {
     return (
         <div className="container">
             <div className="max-w-4xl mx-auto relative mt-25">
-                {/* Main Image */}
                 <img
                     src={room.gallery[currentIndex]}
                     alt="Room"
                     className="w-full h-[450px] object-cover rounded-xl shadow-lg transition-all duration-500"
                 />
-
-                {/* Dots */}
                 <div className="flex justify-center mt-3 space-x-2">
                     {room.gallery.map((_, i) => (
                         <button
                             key={i}
                             onClick={() => setCurrentIndex(i)}
-                            className={`w-3 h-3 rounded-full transition-all ${i === currentIndex
-                                    ? "bg-black"
-                                    : "bg-gray-400"
-                                }`}
+                            className={`w-3 h-3 rounded-full transition-all ${i === currentIndex ? "bg-black" : "bg-gray-400"}`}
                         ></button>
                     ))}
                 </div>
-
-
                 <div>
                     <h1 className="font-bold text-center mt-5 text-3xl">{room.type} Room</h1>
                     <Form />
                 </div>
             </div>
-            
-
         </div>
     );
 }
